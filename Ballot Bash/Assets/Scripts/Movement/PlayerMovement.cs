3f2ac245df8ffca0007a618240d525866c046671
Squizyton <fallendemongod@gmail.com>
2019-10-30 17:15:08 -0400

    Let's Try This Again
    Hi

---
+++ Ballot Bash/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;                //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+    }
+}
