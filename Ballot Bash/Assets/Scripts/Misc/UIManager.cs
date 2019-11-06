5e3f87fa81b86358c94df6157b61a8377625b9c2
Squizyton <fallendemongod@gmail.com>
2019-11-06 13:45:21 -0500

    Test Scene Redone
    Test scene was redone

--- Ballot Bash/Assets/Scripts/Misc/UIManager.cs
+++ Ballot Bash/Assets/Scripts/Misc/UIManager.cs
@@ -8,13 +8,17 @@ public class UIManager : MonoBehaviour
     public Text timerText;
 
     
-    private float Timer;
+    public float Timer;
 
+    public bool CountUp, CountDown;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (CountUp)
+        {
+            Timer = 0;
+        }
 
 
     }
@@ -22,11 +26,34 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float t = Time.time - Timer;
+        float t = Timer - Time.time ;
 
 
-        string minutes = ((int)t / 60).ToString(); 
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+
+    }
+    void TimerCountUp()
+    {
+        float t = Timer - Time.time;
 
 
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+
+    void TimerCountDown()
+    {
+        float t = Timer - Time.time;
+
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
     }
 }
