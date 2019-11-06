54eb7a3356a3b10c7a5b4f6549f51f9856daefce
JackofAllTradez7 <57196083+JackofAllTradez7@users.noreply.github.com>
2019-11-06 13:08:39 -0500

    github please

--- Ballot Bash/Assets/Scripts/Misc/UIManager.cs
+++ Ballot Bash/Assets/Scripts/Misc/UIManager.cs
@@ -1,60 +1,32 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
-    //timer things
-    public Text timerText;
-    public float Timer;
 
-    public bool CountUp, CountDown;
+    public Text timerText;
+
+    
+    private float Timer;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        if (CountUp)
-        {
-            Timer = 0;
-        }
+        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CountUp)
-        {
-            TimerCountUp();
-        }
-        else if (CountDown)
-        {
-            TimerCountDown();
-        }
-    }
+        float t = Time.time - Timer;
 
-   void TimerCountDown()
-    {
-        
-        float t = Timer - Time.time;
 
-        string minutes = ((int)t / 60).ToString();
+        string minutes = ((int)t / 60).ToString(); 
 
-        string seconds = (t % 60).ToString("f2");
 
-        timerText.text = minutes + ":" + seconds;
-    }
-
-    void TimerCountUp()
-    {
-
-        float t = Timer + Time.time;
-
-        string minutes = ((int)t / 60).ToString();
-
-        string seconds = (t % 60).ToString("f2");
-
-        timerText.text = minutes + ":" + seconds;
     }
 }
