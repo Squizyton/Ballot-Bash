daa4b6dbf3bd887567b93352bbdca955e3c6744b
Hanson <jmhanson2@labs.neit.edu>
2019-12-04 15:08:05 -0500

    Cars. vroom vroom

---
+++ Ballot Bash/Assets/Scripts/Misc/CarMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public bool goRight, goLeft;
+
+
+     public int speed;
+
+    Rigidbody rb;
+
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goRight)
+        {
+           rb.velocity = -transform.forward * speed;
+        }
+        else if (goLeft)
+        {
+            rb.velocity = transform.forward * speed;
+        }
+    }
+
+}
---
+++ Ballot Bash/Assets/Scripts/Misc/SpawnACar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnACar : MonoBehaviour
+{
+
+
+    public GameObject[] Cars;
+
+    public GameObject spawnedCar;
+
+    public int rotation;
+
+    public int CarSpeed;
+
+    public bool goRight, goLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+
+    IEnumerator Timer()
+    {
+       int timer = Random.Range(1, 8);
+
+        Debug.Log(timer);
+        yield return new WaitForSeconds(timer);
+
+        Spawn();
+    }
+
+
+    void Spawn()
+    {
+
+        spawnedCar = Instantiate(Cars[Random.Range(0, Cars.Length)], transform.position, transform.rotation);
+        if (goRight)
+        {
+            spawnedCar.GetComponent<CarMovement>().goRight = true;
+            spawnedCar.GetComponent<CarMovement>().speed = CarSpeed;
+        }
+        else if(goLeft)
+        {
+
+            spawnedCar.GetComponent<CarMovement>().goLeft = true;
+            spawnedCar.GetComponent<CarMovement>().speed = CarSpeed;
+        }
+
+        StartCoroutine(Timer());
+        Debug.Log("loop");
+    }
+
+    
+
+}
